174d95109e823ea41f2084087cc503f29ba290d2
Tran Hung Quoc <quoc911ndu1819@gmail.com>
2024-09-17 10:32:16 +0700

    first commit

---
+++ src/main/java/com/project/cnpm/CnpmApplication.java
@@ -0,0 +1,13 @@
+package com.project.cnpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnpmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/cnpm/CnpmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cnpm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnpmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
